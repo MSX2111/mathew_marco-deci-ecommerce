263ea5b8df3f728c4bec705c3a2209fc900e9ac9
MSX2111 <sonicexesonicexe780@gmail.com>
2026-08-23 14:58:12 +0300

    removed secrets

--- server/server.js
+++ server/server.js
@@ -1,33 +1,8 @@
-import express from "express";
-import cors from "cors";
 import "dotenv/config";
+import app from "./app.js";
 
-import userRoutes from "./routes/user.routes.js";
-import productRotes from "./routes/product.routes.js";
-import cartRoutes from "./routes/cart.routes.js";
-import reviewRoutes from "./routes/review.routes.js";
-import activityLogRoutes from "./routes/activityLog.routes.js";
-import connectMongoDB from "./config/mongodb.js";
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-connectMongoDB();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/user", userRoutes);
-app.use("/products", productRotes);
-app.use("/cart", cartRoutes);
-app.use("/reviews", reviewRoutes);
-app.use("/activity-logs", activityLogRoutes);
-
-// 404
-app.use((req, res) => {
-  res.status(404).json({
-    message: "API route not found",
-  });
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
